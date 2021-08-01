785c46a5b6e05db43091ef744631db1dbecdc877
Andreev Danila <mail@neandreev.ru>
2021-08-01 14:49:24 +0500

    fix duplicateRss

--- src/validateSchema.js
+++ src/validateSchema.js
@@ -1,10 +1,9 @@
-import i18next from 'i18next';
 import * as yup from 'yup';
 
 export default (state) => yup.string().url().required()
   .test({
     name: 'isDuplicate',
-    message: i18next.t('feedback.duplicateUrl'),
+    message: { key: 'feedback.duplicateUrl' },
     test: (value) => {
       const urls = state.feeds.map(({ url }) => url);
       return !urls.includes(value);
