e8f72e621671f29c8e4df51f886ccf8b90204545
Andreev Danila <mail@neandreev.ru>
2021-08-01 15:37:51 +0500

    remove unnecessary dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
